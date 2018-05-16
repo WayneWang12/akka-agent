4bee31c97bf5c90a89b6d0c2f1915193a06d4b9e
scweang <scweang@hotmail.com>
2018-05-17 01:53:50 +0800

    1比8比9 减少线程池大小。

--- mesh-agent/src/main/scala/mesh/RequestHandler.scala
+++ mesh-agent/src/main/scala/mesh/RequestHandler.scala
@@ -24,7 +24,7 @@ class RequestHandler(implicit materializer: Materializer) extends Actor with Act
           1
         case 30001 =>
 //          tcp.throttle(80, 55.millis)
-          6
+          8
         case 30002 =>
 //          tcp.throttle(150, 55.millis)
           9
