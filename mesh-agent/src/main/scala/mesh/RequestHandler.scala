c26f4d7c392671da592a3604e1d5b8288a494364
scweang <scweang@hotmail.com>
2018-05-26 01:18:47 +0800

    partion代替balance

--- mesh-agent/src/main/scala/mesh/RequestHandler.scala
+++ mesh-agent/src/main/scala/mesh/RequestHandler.scala
@@ -15,60 +15,53 @@ class RequestHandler(implicit materializer: Materializer) extends Actor with Act
   context.system.eventStream.subscribe(self, classOf[EndpointsUpdate])
 
   def endpointsFlow(endpoints: Set[Endpoint]) = {
-    val tcpFlows = endpoints.toList.sortBy(_.scale).flatMap { endpoint =>
+    val tcpFlows = endpoints.toList.sortBy(_.scale).map { endpoint =>
       val tcp = Flow[ByteString].buffer(20, OverflowStrategy.backpressure).via(Tcp().outgoingConnection(endpoint.host, endpoint.port))
-      endpoint.scale match {
-        case ProviderScale.Small =>
-          List.fill(1)(tcp)
-        case ProviderScale.Medium =>
-          List.fill(1)(tcp)
-        case ProviderScale.Large =>
-          List.fill(1)(tcp)
-      }
+      tcp
     }
 
-    def proportional[T]:T => Int = {
-      var i = -1
-      t => {
-        i += 1
-        if(i == 0) i
-        else if(i <= 4) 1
-        else if(i <= 8) 2
-        else {
-          i = -1
-          2
-        }
+  def proportional[T]: T => Int = {
+    var i = -1
+    _ => {
+      i += 1
+      if (i < 0) i
+      else if (i <= 4) 1
+      else if (i <= 8) 2
+      else {
+        i = -1
+        2
       }
     }
-
-    Flow.fromGraph(GraphDSL.create() { implicit builder =>
-      import GraphDSL.Implicits._
-      val balancer = builder.add(Partition[ByteString](tcpFlows.size, proportional))
-      val merge = builder.add(Merge[ByteString](tcpFlows.size))
-      tcpFlows.foreach { tcp =>
-        balancer ~> tcp.async ~> merge
-      }
-      FlowShape(balancer.in, merge.out)
-    })
   }
 
-  def getSourceByEndpoints(endpoints: Set[Endpoint]): SourceQueueWithComplete[(Long, ByteString)] = {
-    val handleFlow = Flow[(Long, ByteString)]
-      .via(DubboFlow.connectionIdFlow)
-      .via(endpointsFlow(endpoints))
-      .via(Framing.lengthField(4, 12, 64 * 1024, ByteOrder.BIG_ENDIAN))
-      .to(DubboFlow.decoder)
-    Source.queue[(Long, ByteString)](256, OverflowStrategy.backpressure)
-      .to(handleFlow).run()
-  }
+  Flow.fromGraph(GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+    val balancer = builder.add(Partition[ByteString](tcpFlows.size, proportional))
+    val merge = builder.add(Merge[ByteString](tcpFlows.size))
+    tcpFlows.foreach { tcp =>
+      balancer ~> tcp.async ~> merge
+    }
+    FlowShape(balancer.in, merge.out)
+  })
+}
+
+def getSourceByEndpoints (endpoints: Set[Endpoint] ): SourceQueueWithComplete[(Long, ByteString)] = {
+  val handleFlow = Flow[(Long, ByteString)]
+  .via (DubboFlow.connectionIdFlow)
+  .via (endpointsFlow (endpoints) )
+  .via (Framing.lengthField (4, 12, 64 * 1024, ByteOrder.BIG_ENDIAN) )
+  .to (DubboFlow.decoder)
+  Source.queue[(Long, ByteString)] (256, OverflowStrategy.backpressure)
+  .to (handleFlow).run ()
+}
 
   var source: SourceQueueWithComplete[(Long, ByteString)] = _
 
   override def receive: Receive = {
-    case (cid: Long, bs: ByteString) =>
-      source.offer(cid -> bs)
-    case EndpointsUpdate(newEndpoints) =>
-      log.info(s"start new source for endpoints $newEndpoints")
-      source = getSourceByEndpoints(newEndpoints)
-  }
+  case (cid: Long, bs: ByteString) =>
+  source.offer (cid -> bs)
+  case EndpointsUpdate (newEndpoints) =>
+  log.info (s"start new source for endpoints $newEndpoints")
+  source = getSourceByEndpoints (newEndpoints)
+}
 }
